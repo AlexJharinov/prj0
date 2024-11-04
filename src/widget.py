5ddd084ca68a60a54e54175b8e53d11d0e339508
Alex <magwai1@yandex.ru>
2024-11-04 17:32:55 +0400

    Создание функции mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -5,13 +5,13 @@ from src.masks import get_mask_card_number
 def mask_account_card(card_inf: str) -> str:
     """Функция возвращает строку с замаскированным номером"""
     if "Счет" in card_inf:
-        return f"Cчет {get_mask_account(card_inf[-16:])}"
+        return card_inf.replace(card_inf[-20:], get_mask_account(card_inf[-16:]))
     else:
         return card_inf. replace(card_inf[-16:], get_mask_card_number(card_inf[-16:]))
-
+card = "Счет 73654108430135874305"
+print( mask_account_card(card))
 
 def get_date(data_inf: str) -> str:
     """Возвращает информацию о дате в формате число.месяц.год"""
-
     return f"{data_inf[8:10]}.{data_inf[5:7]}.{data_inf[0:4]}"
 
