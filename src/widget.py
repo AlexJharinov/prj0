b4a9423cf1b89abab556b304bcc29d66b7164ea5
Alex <magwai1@yandex.ru>
2024-11-04 14:23:46 +0400

    Создана функция get_data и тело функции mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,15 @@
+def mask_account_card(card_inf: str) -> str:
+    """Функция возвращает строку с замаскированным номером"""
+
+
+
+
+
+
+def get_date(data_inf: str) -> str:
+    """Возвращает информацию о дате в формате число.месяц.год"""
+
+    return f"{data_inf[8:10]}.{data_inf[5:7]}.{data_inf[0:4]}"
+
+
+
