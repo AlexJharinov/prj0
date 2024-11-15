f85a213061670fe97ec2a05a65c7f97a22f950e5
Alex <magwai1@yandex.ru>
2024-11-15 18:58:56 +0400

    creating a filter_by_state, sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,14 @@
-def filter_by_state(dict:):
-    """Фильтрует список словарей по значению ключа """
-    pass
+from typing import Any
 
-def sort_by_date(dict:):
-    """Возвращает новый список отсортированный по дате"""
-    pass
+def filter_by_state(data: list[dict[str, Any]], state: str="EXECUTED") -> list[dict[str, Any]]:
+    """Фильтрует список словарей по значению ключа 'state'"""
+    list_sort = []
+    for i in data:
+      if i['state'] == state:
+        list_sort.append(i)
+    return list_sort
+
+def sort_by_date(data: list[dict[str, Any]], reverse: bool=True) -> list[dict[str, Any]]:
+    """Сортирует список словарей по значению ключа 'date' """
+
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
