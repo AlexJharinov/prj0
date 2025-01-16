f5ae0b0f085748c80b43a84d9deb619417967f21
magwai <neftkom.otk@gmail.com>
2025-01-16 21:21:01 +0000

    Написана функция def json_read, добавлен файл с транзакциями operations.json

--- src/utils.py
+++ src/utils.py
@@ -7,19 +7,19 @@ def json_read(file_):
     Принимает на вход путь до JSON-файла, возврщает данные о финансовых транзакциях
     """
     operations_list = []
+    try:
+        with open(file_, 'r', encoding='utf8') as f:
+            data = json.load(f)
+        return data
 
-    with open(file_, 'r', encoding='utf8') as f:
-        data = json.load(f)
-    return
-
-
-
-
-
+    except Exception:
+        return operations_list
 
 
 
-def conversion_value():
+def conversion_value(transaction):
+    if transaction['operationAmount']['currency']['code'] == ('RUB')
+        return transaction['operationAmount']['amount']
     pass
 
 
