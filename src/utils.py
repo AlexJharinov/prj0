e062e0d208cd024b88630dbf486d854cb91a0315
magwai <neftkom.otk@gmail.com>
2025-01-19 19:02:31 +0000

    Дописаны функции def conversion_value, def convert_amount.

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 import json
+import os
+from http.client import responses
 
+import requests
+from dotenv import load_dotenv
 
 
 def json_read(file_):
@@ -18,8 +22,29 @@ def json_read(file_):
 
 
 def conversion_value(transaction):
-    if transaction['operationAmount']['currency']['code'] == ('RUB')
+    """
+    Принмает транзакцию, возвращает ее сумму
+    """
+    if transaction['operationAmount']['currency']['code'] == 'RUB':
         return transaction['operationAmount']['amount']
-    pass
 
+    elif transaction['operationAmount']['currency']['code'] != 'RUB':
+        conv_currency = convert_amount(transaction['operationAmount']['amount'], transaction['operationAmount']['currency']['code'])
 
+        return conv_currency
+
+def convert_amount(amount, currency):
+    """
+    Принимает сумму и валюту, возвращает сумму в Руб
+    """
+
+    load_dotenv()
+
+    api_key = os.getenv("API_KEY")
+    headdiers = {"api_key": api_key}
+
+    payload = {}
+    url =
+    response = requests.request("GET", url, headers=headers, data=payload)
+    result = response.json()["result"]
+    return result
